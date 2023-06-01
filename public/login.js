16442567561b21ffe47c01376f2abba62f172a9e
Nathan <nbn1343@gmail.com>
2023-06-01 15:43:05 -0600

    Calling third party endpoint

--- public/login.js
+++ public/login.js
@@ -1,4 +1,24 @@
 function login() {
     const nameEl = document.querySelector("#name");
     localStorage.setItem("userName", nameEl.value);
-    window.location.href = "play.html";
+    window.location.href = "play.html"; };
+
+function displayQuote(data) {
+    fetch('https://api.quotable.io/random')
+        .then((response) => response.json())
+        .then((data) => {
+        const containerEl = document.querySelector('#quote');
+          
+        const quoteEl = document.createElement('p');
+        quoteEl.classList.add('quote');
+        const authorEl = document.createElement('p');
+        authorEl.classList.add('author');
+          
+        quoteEl.textContent = data.content;
+        authorEl.textContent = data.author;
+          
+        containerEl.appendChild(quoteEl);
+        containerEl.appendChild(authorEl);
+        });
+    }      
+      displayQuote();
--- public/play.js
+++ public/play.js
@@ -9,8 +9,50 @@ let movesNum;
 let movescell = document.getElementById('movesnum');
 let tiles = document.getElementsByClassName('Tile');
 let emptyRow, emptyCol;
+let scores = [];
 
-let randomizePuzzle = function () {
+function updateScoreboard() {
+  const scoreboard = document.getElementById('scoreboard');
+  scoreboard.innerHTML = '';
+
+  scores.sort((a, b) => a.moves - b.moves); // Sort scores in ascending order
+
+  for (let i = 0; i < scores.length; i++) {
+    const score = scores[i];
+    const scoreElement = document.createElement('div');
+    scoreElement.textContent = `${score.playerName}: ${score.moves} moves`;
+    scoreboard.appendChild(scoreElement);
+  }
+}
+
+let checkPuzzleCompletion = function () {
+  let isCompleted = true;
+  for (let i = 0; i < 15; i++) {
+    let correctRow = Math.ceil((i + 1) / 4);
+    let correctCol = ((i + 1) % 4) || 4;
+    let currentRow = parseInt(tiles[i].style.gridRow);
+    let currentCol = parseInt(tiles[i].style.gridColumn);
+
+    if (currentRow !== correctRow || currentCol !== correctCol) {
+      isCompleted = false;
+      break;
+    }
+  }
+
+  if (isCompleted) {
+    displayMessage('System', 'Congratulations! Puzzle completed.');
+    saveScore(storedUsername, movesNum);
+    updateScoreboard();
+  }
+};
+
+function saveScore(playerName, moves) {
+  let scoreData = JSON.parse(localStorage.getItem('scores')) || []; // Retrieve existing scores or initialize as empty array
+  scoreData.push({ playerName, moves });
+  localStorage.setItem('scores', JSON.stringify(scoreData));
+}
+
+function randomizePuzzle() {
   movesNum = 0;
   movescell.innerHTML = movesNum;
   [emptyRow, emptyCol] = [4, 4];
@@ -39,9 +81,9 @@ let randomizePuzzle = function () {
     tiles[i].style.gridColumn = poppedPos[0][1];
   }
   checkPuzzleCompletion();
-};
+}
 
-let solvePuzzle = function () {
+function solvePuzzle() {
   movesNum = 0;
   movescell.innerHTML = movesNum;
   [emptyRow, emptyCol] = [4, 4];
@@ -50,9 +92,9 @@ let solvePuzzle = function () {
     tiles[i].style.gridColumn = ((i + 1) % 4) || 4;
   }
   checkPuzzleCompletion();
-};
+}
 
-let moveTile = function () {
+function moveTile() {
   let thisRow = parseInt(this.style.gridRow);
   let thisCol = parseInt(this.style.gridColumn);
 
@@ -65,33 +107,7 @@ let moveTile = function () {
     movescell.innerHTML = movesNum;
     checkPuzzleCompletion();
   }
-};
-
-let checkPuzzleCompletion = function () {
-  let isCompleted = true;
-  for (let i = 0; i < 15; i++) {
-    let correctRow = Math.ceil((i + 1) / 4);
-    let correctCol = ((i + 1) % 4) || 4;
-    let currentRow = parseInt(tiles[i].style.gridRow);
-    let currentCol = parseInt(tiles[i].style.gridColumn);
-
-    if (currentRow !== correctRow || currentCol !== correctCol) {
-      isCompleted = false;
-      break;
-    }
-  }
-
-  if (isCompleted) {
-    displayMessage('System', 'Congratulations! Puzzle completed.');
-    saveScore(storedUsername, movesNum);
-    updateScoreboard();
-  }
-};
-
-function saveScore(playerName, moves) {
-    scores.push({ playerName, moves });
-    localStorage.setItem('scores', JSON.stringify(scores));
-  }
+}
 
 for (let i = 0; i < 15; i++) {
   tiles[i].addEventListener('click', moveTile);
@@ -144,27 +160,27 @@ function simulateReply() {
     'Hi there!',
     'How are you doing today?',
     'What have you been up to lately?',
-    'Nice weather were having, isnt it?',
+    'Nice weather we\'re having, isn\'t it?',
     'Do you have any fun plans for the weekend?',
     'I recently watched a great movie. Have you seen any good movies lately?',
-    'Whats your favorite type of music?',
-    'Im really into cooking. Do you enjoy cooking as well?',
-    'Im planning a trip to a new city. Any recommendations?',
-    'Im a big fan of hiking. Do you have any favorite outdoor activities?',
+    'What\'s your favorite type of music?',
+    'I\'m really into cooking. Do you enjoy cooking as well?',
+    'I\'m planning a trip to a new city. Any recommendations?',
+    'I\'m a big fan of hiking. Do you have any favorite outdoor activities?',
     'What do you do for a living?',
     'Have you tried any new restaurants recently?',
     'Do you have any pets?',
     'I love reading books. Any book recommendations?',
-    'Whats the last concert you attended?',
+    'What\'s the last concert you attended?',
     'I enjoy playing video games. Are you a gamer?',
-    'Im thinking about starting a new hobby. Any suggestions?',
-    'Whats your favorite TV show?',
-    'I love traveling. Whats the most memorable trip youve taken?',
+    'I\'m thinking about starting a new hobby. Any suggestions?',
+    'What\'s your favorite TV show?',
+    'I love traveling. What\'s the most memorable trip you\'ve taken?',
     'Do you follow any sports teams?',
-    'Im learning a new language. Have you ever tried learning a foreign language?',
+    'I\'m learning a new language. Have you ever tried learning a foreign language?',
     'Do you enjoy going to art galleries or museums?',
-    'Im looking for new podcasts to listen to. Any recommendations?',
-    'Whats your go-to exercise or workout routine?',
+    'I\'m looking for new podcasts to listen to. Any recommendations?',
+    'What\'s your go-to exercise or workout routine?',
     'I enjoy trying out new recipes. Do you have a favorite dish?',
   ];
   setInterval(() => {
@@ -173,5 +189,6 @@ function simulateReply() {
     displayMessage(randomSender, randomMessage);
   }, Math.random() * 4000 + 2000);
 }
+
 
 simulateReply();
