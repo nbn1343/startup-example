42c9e2dbcc11c03a22105ac11d5230f563687638
Nathan <nbn1343@gmail.com>
2023-06-01 11:26:43 -0600

    Converting startup to a service

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 
-const usernameInput = document.getElementById('username');
+const express = require('express');
+const app = express();
 
-document.querySelector('form').addEventListener('submit', function(event) {
+// The service port. In production the frontend code is statically hosted by the service on the same port.
+const port = process.argv.length > 2 ? process.argv[2] : 3000;
 
-  event.preventDefault();
+// JSON body parsing using built-in middleware
+app.use(express.json());
 
-  const username = usernameInput.value;
+// Serve up the frontend static content hosting
+app.use(express.static('public'));
 
-  localStorage.setItem('username', username);
+// Router for service endpoints
+const apiRouter = express.Router();
+app.use(`/api`, apiRouter);
 
-  window.location.href = 'play.html';
+// GetScores
+apiRouter.get('/scores', (_req, res) => {
+  res.send(scores);
+});
+
+// SubmitScore
+apiRouter.post('/score', (req, res) => {
+  scores = updateScores(req.body, scores);
+  res.send(scores);
+});
+
+// Return the application's default page if the path is unknown
+app.use((_req, res) => {
+  res.sendFile('index.html', { root: 'public' });
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
