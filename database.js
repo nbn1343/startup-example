4c083b04100e7f745ee6d4e5f12562303345b268
Nathan <nbn1343@gmail.com>
2023-06-06 20:27:01 -0600

    Updating database

--- database.js
+++ database.js
@@ -3,10 +3,9 @@ const config = require('./dbConfig.json');
 
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
-const db = client.db('simon');
+const db = client.db('startup');
 const scoreCollection = db.collection('score');
 
-// This will asynchronously test the connection and exit the process if it fails
 (async function testConnection() {
   await client.connect();
   await db.command({ ping: 1 });
