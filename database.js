29e2a482c099303d9aa01ef35098d8e7b110b3f6
Nathan <nbn1343@gmail.com>
2023-06-08 11:22:50 -0600

    Updating login authentication

--- database.js
+++ database.js
@@ -6,6 +6,7 @@ const config = require('./dbConfig.json');
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
 const db = client.db('startup');
+const userCollection = db.collection('user');
 const scoreCollection = db.collection('score');
 
 (async function testConnection() {
@@ -15,6 +16,29 @@ const scoreCollection = db.collection('score');
   console.log(`Unable to connect to database with ${url} because ${ex.message}`);
   process.exit(1);
 });
+
+function getUser(email) {
+    return userCollection.findOne({ email: email });
+  }
+  
+  function getUserByToken(token) {
+    return userCollection.findOne({ token: token });
+  }
+  
+  async function createUser(email, password) {
+    // Hash the password before we insert it into the database
+    const passwordHash = await bcrypt.hash(password, 10);
+  
+    const user = {
+      email: email,
+      password: passwordHash,
+      token: uuid.v4(),
+    };
+    await userCollection.insertOne(user);
+  
+    return user;
+  }
+  
 
 async function addScore(score) {
   const result = await scoreCollection.insertOne(score);
@@ -31,5 +55,11 @@ function getHighScores() {
   return cursor.toArray();
 }
 
-module.exports = { addScore, getHighScores };
+module.exports = {
+    getUser,
+    getUserByToken,
+    createUser,
+    addScore,
+    getHighScores,
+  };
 
