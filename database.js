9aa5f533eac257a533f486ebd8e2479941e7c533
Nathan <nbn1343@gmail.com>
2023-06-08 11:15:48 -0600

    Adding login authentication

--- database.js
+++ database.js
@@ -1,4 +1,6 @@
 const { MongoClient } = require('mongodb');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
 const config = require('./dbConfig.json');
 
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
