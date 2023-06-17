30410db9656b862d5f9a274faccfa04e324a6c58
Nathan <nbn1343@gmail.com>
2023-06-17 15:33:20 -0600

    Login converted to React

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,12 +1,136 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './login.css';
+// import { Play } from './play/play'
+// import { useHistory } from 'react-router-dom';import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+
+
+
 
 export function Login() {
-    return (
-        <main className='container-fluid bg-secondary text-center'>
-            <h1 class="login-btn">Welcome!</h1>
-        
-        </main>
-    );
+    const navigate = useNavigate();
+
+    useEffect(() => {
+      checkUser();
+    }, []);
+
+  async function checkUser() {
+    const userName = localStorage.getItem('userName');
+    if (userName) {
+      document.querySelector('#playerName').textContent = userName;
+      setDisplay('loginControls', 'none');
+      setDisplay('playControls', 'block');
+    } else {
+      setDisplay('loginControls', 'block');
+      setDisplay('playControls', 'none');
+    }
+  }
+
+  async function loginUser() {
+    loginOrCreate(`/api/auth/login`);
+  }
+
+  async function createUser() {
+    loginOrCreate(`/api/auth/create`);
+  }
+
+  async function loginOrCreate(endpoint) {
+    const userName = document.querySelector('#userName')?.value;
+    const password = document.querySelector('#userPassword')?.value;
+    const response = await fetch(endpoint, {
+      method: 'post',
+      body: JSON.stringify({ email: userName, password: password }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    });
+
+    if (response.ok) {
+      localStorage.setItem('userName', userName);
+      navigate('/play');
+      ;
+    } else {
+      const body = await response.json();
+      const modalEl = document.querySelector('#msgModal');
+      modalEl.querySelector('.modal-body').textContent = `⚠ Error: ${body.msg}`;
+      const msgModal = new bootstrap.Modal(modalEl, {});
+      msgModal.show();
+    }
+  }
+
+  function play() {
+    navigate('/play');
+  }
+
+  function logout() {
+    localStorage.removeItem('userName');
+    fetch(`/api/auth/logout`, {
+      method: 'delete',
+    }).then(() => (window.location.href = '/'));
+  }
+
+  async function getUser(email) {
+    let scores = [];
+    // See if we have a user with the given email.
+    const response = await fetch(`/api/user/${email}`);
+    if (response.status === 200) {
+      return response.json();
+    }
+
+    return null;
+  }
+
+  function setDisplay(id, display) {
+    const element = document.getElementById(id);
+    if (element) {
+      element.style.display = display;
+    }
+  }
+
+  return (
+    <main>
+      <h1 className="login-btn">Welcome!</h1>
+      <div id="loginControls" style={{ display: 'none' }}>
+        <div className="input-group mb-3">
+          <span className="input-group-text">@</span>
+          <input className="form-control" type="text" id="userName" placeholder="your@email.com" />
+        </div>
+        <div className="input-group mb-3">
+          <span className="input-group-text">🔒</span>
+          <input className="form-control" type="password" id="userPassword" placeholder="password" />
+        </div>
+        <button type="button" className="btn btn-primary" onClick={loginUser}>
+          Login
+        </button>
+        <button type="button" className="btn btn-primary" onClick={createUser}>
+          Create
+        </button>
+      </div>
+      <div id="playControls" style={{ display: 'none' }}>
+        <div id="playerName" />
+        <button type="button" className="btn btn-primary" onClick={play}>
+          Play
+        </button>
+        <button type="button" className="btn btn-secondary" onClick={logout}>
+          Logout
+        </button>
+      </div>
+      {/* Error dialog */}
+      <div className="modal fade" id="msgModal" tabIndex={-1}>
+        <div className="modal-dialog modal-dialog-centered">
+          <div className="modal-content text-dark">
+            <div className="modal-body">error message here</div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div id="quote" className="quote-box bg-light text-dark" />
+      <div id="picture" className="picture-box" />
+    </main>
+  );
 }
--- src/play/play.jsx
+++ src/play/play.jsx
@@ -4,6 +4,34 @@ import './play.css';
 
 
 export function Play() {
+  function randomizePuzzle() {
+    movesNum = 0;
+    movescell.innerHTML = movesNum;
+    [emptyRow, emptyCol] = [4, 4];
+    let positions = [
+      [1, 1],
+      [1, 2],
+      [1, 3],
+      [1, 4],
+      [2, 1],
+      [2, 2],
+      [2, 3],
+      [2, 4],
+      [3, 1],
+      [3, 2],
+      [3, 3],
+      [3, 4],
+      [4, 1],
+      [4, 2],
+      [4, 3],
+      [4, 4],
+    ];
+    for (let i = 14; i >= 0; i--) {
+      let r = Math.round(Math.random() * i);
+      let poppedPos = positions.splice(r, 1);
+      tiles[i].style.gridRow = poppedPos[0][0];
+      tiles[i].style.gridColumn = poppedPos[0][1];
+    }
   return (
     <main>
       <figure className="SlidingPuzzleFigure">
@@ -41,4 +69,4 @@ export function Play() {
       </figure>
     </main>
   );
-}
+} }
